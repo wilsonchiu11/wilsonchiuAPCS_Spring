341f3ce00725065f5e9aff867ffe38a8f8dfe4c1
wilsonchiu11 <18chiuw79@millshighsch.org>
2017-03-05 21:33:56 -0800

    3/5/17
    work at home

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -10,8 +10,13 @@ public class TextExcel
 
 	public static void main(String[] args)
 	{
-		
-		
+		Spreadsheet spreadsheet = new Spreadsheet();
+	    Scanner userInput  = new Scanner (System.in);
+	    String input = "";
+	    while(!input.equals ("quit")) {
+	    	input = spreadsheet.processCommand(userInput.next());
+	    		System.out.println(input);
+	    }
 		
 		
 		
